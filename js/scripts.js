d6d4cdcd8b9310801fa51f6db13a9675964ba183
Alan Denison <alan79td@gmail.com>
2016-06-10 13:37:05 -0700

    add styles and front end interface, reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,29 @@
 var pinger = function(number) {
   value = parseInt(number);
-var result = []
+  var result = []
   for(i=1; i<=value; i++) {
     if (i % 3 === 0 && i % 15 != 0) {
-      result.push("ping");
-    }else if (i % 5 === 0 && i % 15 != 0){
-      result.push("pong");
+      result.push("ping" + "<br>");
+    } else if (i % 5 === 0 && i % 15 != 0){
+      result.push("pong" + "<br>");
     } else if (i % 15 === 0) {
-      result.push("pingpong");
+      result.push("pingpong" + "<br>");
     }
     else{
-      result.push(i);
-    }
-  }
+      result.push(i + "<br>");
+    };
+  };
   return(result);
 }
-
-
-
-
-
 
 $(document).ready(function() {
   $(".Ping-form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#user-input").val());
     var result = pinger(userInput);
-
-    $("#result").text(result);
+    $("#result").append(result);
+  });
+  $("#reset").click(function() {
+    $("#result").text("");
   });
 });
