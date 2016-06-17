e5c70a360c9f28972d0ed5b329d8bd7e6dfdeb30
Alan Denison <alan79td@gmail.com>
2016-06-17 13:53:08 -0700

    set rearrange conditional statements, form a column in front end rather than business end

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,14 @@ var pinger = function(number) {
   value = parseInt(number);
   var result = []
   for(i=1; i<=value; i++) {
-    if (i % 3 === 0 && i % 15 != 0) {
-      result.push("ping" + "<br>");
-    } else if (i % 5 === 0 && i % 15 != 0){
-      result.push("pong" + "<br>");
-    } else if (i % 15 === 0) {
-      result.push("pingpong" + "<br>");
-    }
-    else {
-      result.push(i + "<br>");
+    if (i % 15 === 0) {
+      result.push("pingpong");
+    } else if (i % 3 === 0) {
+      result.push("ping");
+    } else if (i % 5 === 0){
+      result.push("pong");
+    } else {
+      result.push(i);
     };
   };
   return(result);
@@ -21,7 +20,7 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = parseInt($("#user-input").val());
     var result = pinger(userInput);
-    $("#result").append(result);
+    $("#result").append(result.join('<br>'));
   });
   $("#reset").click(function() {
     $("#result").text("");
